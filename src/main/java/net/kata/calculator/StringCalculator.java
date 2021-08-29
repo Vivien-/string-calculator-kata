2fd0bf3eeb39217d7b919643d30a128eee503e64
ACHET <vivien.achet@atos.net>
2021-08-30 01:01:28 +0200

    [Step 4 + 5] Allow custom delimiter + add NegativeNumberException

--- src/main/java/net/kata/calculator/StringCalculator.java
+++ src/main/java/net/kata/calculator/StringCalculator.java
@@ -1,23 +1,44 @@
 package net.kata.calculator;
 
+import net.kata.calculator.exceptions.NegativeNumberException;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class StringCalculator {
     private static final List<String> ALLOWED_SEPARATORS = Arrays.asList(",", "\n");
 
-    public int add(String text) {
+    public int add(String text) throws NegativeNumberException {
         if (text == null || text.isBlank()) {
             return 0;
         }
 
-        String[] numbers = text.split(String.join("|", ALLOWED_SEPARATORS));
+        List<String> negatives = new ArrayList<>();
+        String[] numbers = text.split(String.join("|", getDelimiters(text)));
         int result = 0;
 
         for (String str : numbers) {
-            result += parseIntOrZero(str);
+            int value = parseIntOrZero(str);
+            if (value < 0) {
+                negatives.add(str);
+            }
+            result += value;
+        }
+
+        if (!negatives.isEmpty()) {
+            throw new NegativeNumberException("negatives not allowed: " + String.join(" ", negatives));
         }
         return result;
+    }
+
+    private List<String> getDelimiters(String text) {
+        List<String> separators = new ArrayList<>(ALLOWED_SEPARATORS);
+        String[] tokens = text.split("\n");
+        if (tokens.length > 0 && tokens[0].startsWith("//")) {
+            separators.add(tokens[0].replace("//", ""));
+        }
+        return separators;
     }
 
     private int parseIntOrZero(String str) {
--- src/test/java/net/kata/calculator/StringCalculatorTest.java
+++ src/test/java/net/kata/calculator/StringCalculatorTest.java
@@ -1,14 +1,16 @@
 package net.kata.calculator;
 
+import net.kata.calculator.exceptions.NegativeNumberException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class StringCalculatorTest {
     StringCalculator calculator = new StringCalculator();
 
     @Test
-    void step1Tests() {
+    void step1Tests() throws NegativeNumberException {
         assertEquals(0, calculator.add(""));
         assertEquals(0, calculator.add("0"));
         assertEquals(63, calculator.add("63"));
@@ -20,7 +22,7 @@ class StringCalculatorTest {
     }
 
     @Test
-    void step2Tests() {
+    void step2Tests() throws NegativeNumberException {
         assertEquals(3, calculator.add("1,1,1"));
         assertEquals(4, calculator.add("1,1,1,foo,1"));
         assertEquals(1, calculator.add("foo,bar,baz,1"));
@@ -28,9 +30,27 @@ class StringCalculatorTest {
     }
 
     @Test
-    void step3Tests() {
+    void step3Tests() throws NegativeNumberException {
         assertEquals(6, calculator.add("1\n2,3"));
         assertEquals(4, calculator.add("1,\n2,1"));
+    }
+
+    @Test
+    void step4Tests() throws NegativeNumberException {
+        assertEquals(3, calculator.add("//;\n1;2"));
+        assertEquals(2, calculator.add("//2\n1221"));
+        assertEquals(1, calculator.add("//2\n122"));
+        assertEquals(3, calculator.add("//2\n12\n1,1"));
+    }
+
+    @Test
+    void step5Tests() {
+        assertThrows(NegativeNumberException.class, () -> calculator.add("-1,1"));
+        try {
+            calculator.add("-1,-2,-3,4,-5");
+        } catch (NegativeNumberException e) {
+            assertEquals("negatives not allowed: -1 -2 -3 -5", e.getMessage());
+        }
     }
 
 }
