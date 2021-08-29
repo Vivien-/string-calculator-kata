f6045e4f28bfc24a5b873f0c87f08fe10880aafd
ACHET <vivien.achet@atos.net>
2021-08-30 00:01:06 +0200

    [Step 0 - initial repo commit]

---
+++ src/main/java/string/calculator/StringCalculator.java
@@ -0,0 +1,12 @@
+package string.calculator;
+
+public class StringCalculator {
+    public int add(String text) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
